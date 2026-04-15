1d9ac98a1e73e3a8523d6951263b9387ae930fb4
rjahn <rene.jahn@sibvisions.com>
2026-04-15 10:14:34 +0200

    fixed replacemenet

--- 1.10.1/bootstrap.sh
+++ 1.10.1/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Fix error pages
-sed -E -i 's|return 503;|include /etc/nginx/vhost.d/default;\n    location / {\n        return 503;\n    }|g' nginx.tmpl
+# error pages
+sed -i ':a;N;$!ba;s|location \^~ / {\n\s*return 503;\n\s*}|include /etc/nginx/vhost.d/default;\n    location / {\n        return 503;\n    }|g' nginx.tmpl
